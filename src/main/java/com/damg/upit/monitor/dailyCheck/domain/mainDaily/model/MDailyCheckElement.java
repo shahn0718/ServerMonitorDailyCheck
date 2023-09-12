b42dac705fff9058be83351c4c155c0ecaf65843
안상형 <shahn0718@gmail.com>
2023-09-12 18:03:48 +0900

    20230912 etcDaily

--- src/main/java/com/damg/upit/monitor/dailyCheck/domain/mainDaily/model/MDailyCheckElement.java
+++ src/main/java/com/damg/upit/monitor/dailyCheck/domain/mainDaily/model/MDailyCheckElement.java
@@ -4,12 +4,13 @@ public class MDailyCheckElement {
 
     public static final String GW = "GW";
     public static final String GW_KOR = "지니어스";
+
     public static final String ERP = "ERP";
     public static final String ERP_KOR = "동아일보 ERP";
     public static final String HR = "HR";
     public static final String HR_KOR = "인사시스템";
-    public static final String ETC_ERP = "ETC_ERP";
-    public static final String ETC_ERP_KOR = "재무시스템";
+    public static final String ETC_ERP = "ERP_ETC";
+    public static final String ETC_ERP_KOR = "채널A 재무 및 타시스템";
     public static final String INFRA  = "INFRA";
     public static final String INFRA_KOR = "인프라";
 }
