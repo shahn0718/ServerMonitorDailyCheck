170bcbee7120cb72730ac756a0f5c1f6bb8b11dd
안상형 <shahn0718@gmail.com>
2023-11-29 18:08:13 +0900

    20231129 페이지네이션

--- src/main/java/com/damg/upit/monitor/dailyCheck/domain/mainServerMonitor/model/MXmlServerName.java
+++ src/main/java/com/damg/upit/monitor/dailyCheck/domain/mainServerMonitor/model/MXmlServerName.java
@@ -1,12 +1,10 @@
 package com.damg.upit.monitor.dailyCheck.domain.mainServerMonitor.model;
 
 public enum MXmlServerName {
-
     /**
      * GW XML NAME
      *
      */
-
 //    public static final String gwNode1 = "gw-node1";
 //    public static final String gwNode2 = "gw-node2";
 //    public static final String gwNode3 = "gw-node3";
@@ -14,6 +12,5 @@ public enum MXmlServerName {
 //    public static final String gwDevNode1 = "gw-dev-node1";
 //    public static final String gwBackUp = "gw-backup";
 //    public static final String gwArch5 = "gw-arch5";
-
 
 }
